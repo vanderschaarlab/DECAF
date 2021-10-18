79658d21c433dc36d14292888ab10fd35322e2f9
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-10-18 15:11:19 +0300

    add dataloader for adult dataset

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Tuple
 
 import networkx as nx
 import numpy as np
@@ -46,3 +46,149 @@ def gen_data_nonlinear(
     g = np.swapaxes(g, 0, 1)
 
     return pd.DataFrame(g, columns=list(map(str, list_vertex)))
+
+
+def load_adult() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    path = "test_data/adult.csv"
+    names = [
+        "age",
+        "workclass",
+        "fnlwgt",
+        "education",
+        "education-num",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "capital-gain",
+        "capital-loss",
+        "hours-per-week",
+        "native-country",
+        "label",
+    ]
+    df = pd.read_csv(path, names=names, index_col=False)
+    df = df.applymap(lambda x: x.strip() if type(x) is str else x)
+
+    for col in df:
+        if df[col].dtype == "object":
+            df = df[df[col] != "?"]
+
+    replace = [
+        [
+            "Private",
+            "Self-emp-not-inc",
+            "Self-emp-inc",
+            "Federal-gov",
+            "Local-gov",
+            "State-gov",
+            "Without-pay",
+            "Never-worked",
+        ],
+        [
+            "Bachelors",
+            "Some-college",
+            "11th",
+            "HS-grad",
+            "Prof-school",
+            "Assoc-acdm",
+            "Assoc-voc",
+            "9th",
+            "7th-8th",
+            "12th",
+            "Masters",
+            "1st-4th",
+            "10th",
+            "Doctorate",
+            "5th-6th",
+            "Preschool",
+        ],
+        [
+            "Married-civ-spouse",
+            "Divorced",
+            "Never-married",
+            "Separated",
+            "Widowed",
+            "Married-spouse-absent",
+            "Married-AF-spouse",
+        ],
+        [
+            "Tech-support",
+            "Craft-repair",
+            "Other-service",
+            "Sales",
+            "Exec-managerial",
+            "Prof-specialty",
+            "Handlers-cleaners",
+            "Machine-op-inspct",
+            "Adm-clerical",
+            "Farming-fishing",
+            "Transport-moving",
+            "Priv-house-serv",
+            "Protective-serv",
+            "Armed-Forces",
+        ],
+        [
+            "Wife",
+            "Own-child",
+            "Husband",
+            "Not-in-family",
+            "Other-relative",
+            "Unmarried",
+        ],
+        ["White", "Asian-Pac-Islander", "Amer-Indian-Eskimo", "Other", "Black"],
+        ["Female", "Male"],
+        [
+            "United-States",
+            "Cambodia",
+            "England",
+            "Puerto-Rico",
+            "Canada",
+            "Germany",
+            "Outlying-US(Guam-USVI-etc)",
+            "India",
+            "Japan",
+            "Greece",
+            "South",
+            "China",
+            "Cuba",
+            "Iran",
+            "Honduras",
+            "Philippines",
+            "Italy",
+            "Poland",
+            "Jamaica",
+            "Vietnam",
+            "Mexico",
+            "Portugal",
+            "Ireland",
+            "France",
+            "Dominican-Republic",
+            "Laos",
+            "Ecuador",
+            "Taiwan",
+            "Haiti",
+            "Columbia",
+            "Hungary",
+            "Guatemala",
+            "Nicaragua",
+            "Scotland",
+            "Thailand",
+            "Yugoslavia",
+            "El-Salvador",
+            "Trinadad&Tobago",
+            "Peru",
+            "Hong",
+            "Holand-Netherlands",
+        ],
+        [">50K", "<=50K"],
+    ]
+
+    for row in replace:
+        df = df.replace(row, range(len(row)))
+
+    df = df.values
+    X = df[:, :14].astype(np.uint32)
+    y = df[:, 14].astype(np.uint8)
+
+    return X, y
